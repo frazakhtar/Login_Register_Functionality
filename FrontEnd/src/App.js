91adedb9d3b95ff195a5655faaeeb1e21f852aa3
frazakhtar <87436611+frazakhtar@users.noreply.github.com>
2024-09-21 14:42:10 +0530

    frontend changes

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import LoginForm from './Components/LoginForm/LoginForm';
+import RegisterForm from './Components/RegisterForm/RegisterForm';
+
+function App() {
+  return (
+    <div className="App">
+         <LoginForm />
+         <br />
+         <RegisterForm />
+
+    </div>
+  );
+}
+
+export default App;
